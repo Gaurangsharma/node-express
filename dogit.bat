ee5a009c71003554f6a5d295fa04dab6fa32685d
gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2019-09-26 01:09:37 +0530

    Expess Example

---
+++ dogit.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Expess Example"
+git push origin master
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express=require('express');
 const http=require('http');
+const morgan=require('morgan');
 const hostname='localhost';
 const port=3000;
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log(req.header);
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{ 
     res.getHeader("Content-Type","text/html");
     res.end("<html><body><h1>hello Express</h1></body><html>");
 }) 
